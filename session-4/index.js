d5f87693008ca75f335097ba16970e4275be1a15
Nancy Chisenga <nanciechisenga@gmail.com>
2022-01-24 20:17:35 +0000

    creating objects and using operators

--- session-4/index.js
+++ session-4/index.js
@@ -1,17 +1,39 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
 
+let personA = {
+    name: "Sally",
+    age: "45",
+    location: "Wales",
+    likes: "Singing"
+}
+let personB = {
+    name: "Vicki",
+    age: "44",
+    location: "Chesterfield",
+    likes: "Running"
+}
+let personC = {
+    name: "Ezra",
+    age: "26",
+    location: "Sheffield",
+    likes: "Playing football"
+}
+let personD = {
+    name: "Jasmin",
+    age: "27",
+    location: "Chesterfield",
+    likes: "Saving lives"
+}
 
 
 
-
-
-
-
-
-
-
-
-
+function biography(person) {
+    return c
+}
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD));
 
 
 
@@ -19,25 +41,28 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+ const x = 5;
+ const y = 8;
 
-// const addition = x + y;
+ const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+ console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+ const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+ console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+ const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+ console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+ const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division);
 
+ console.log(x * y / x);
+console.log(x % y) // modulus
+console.log(x ** y) // exponentiation or the power of 
 
 
 
