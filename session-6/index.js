afb92cd2aec82efacd4c81f1e558f4e40e7c3602
Nancy Chisenga <nanciechisenga@gmail.com>
2022-01-31 19:31:17 +0000

    while and for loops

--- session-6/index.js
+++ session-6/index.js
@@ -1,8 +1,13 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+//const randomNumber = Math.round(Math.random() * 10);
+let count = 1;
 
 // ****************************************************TASK 1****************************************************
-
+while (count < 10) {
+    const randomNumber = Math.round(Math.random() * 10);
+   count = count + 1
+  console.log('this is a random number ' + randomNumber);
+}
 
 
 
@@ -19,7 +24,10 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 2****************************************************
 
-
+for (let count = 1; count < 10; count++) {
+  const randomNumber = Math.round(Math.random() * 10);
+  console.log('this is count ' + randomNumber);
+}
 
 
 
